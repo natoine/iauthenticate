0b56b5aeb80e244754a8ed5e559b49677a8a52e7
Axel <axel3392@hotmail.fr>
2017-10-10 11:33:26 +0200

    mise à jour fichier prototypage twitter

--- twitter2.js
+++ twitter2.js
@@ -3,10 +3,10 @@ var credentials = require('./config/auth.js');
 
  
 var client = new Twitter({
-    consumer_key: credentials.consumerKey,
-    consumer_secret: credentials.consumerSecret,
-    access_token_key: credentials.accessTokenKey,
-    access_token_secret: credentials.accessTokenSecret
+    consumer_key: credentials.twitterAuth.consumerKey,
+    consumer_secret: credentials.twitterAuth.consumerSecret,
+    access_token_key: credentials.twitterAuth.accessTokenKey,
+    access_token_secret: credentials.twitterAuth.accessTokenSecret
 });
  
 var params = {screen_name: '20Minutes'};
