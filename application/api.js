d5f65a43ecb702727a4171d0d8263f8cf1e9a2b4
natoine <antoineseilles@gmail.com>
2017-11-05 00:06:21 +0100

    now have a middleware route for api calls

--- application/api.js
+++ application/api.js
@@ -14,10 +14,6 @@ module.exports = function(app, express) {
     // API with TOKEN ===============================
     // =====================================
     
-	apiRoutes.get('/', function(req, res) {
-	  	res.json({ message: 'Welcome to the coolest API on earth!' })
-	})
-
 	// route to authenticate a user (POST http://localhost:8080/api/authenticatelocal)
 	// expects an email and password parameter in x-www-form-urlencoded
 	apiRoutes.post('/authenticatelocal', function(req, res) {
@@ -56,6 +52,40 @@ module.exports = function(app, express) {
 
 		    }
 		})
+	})
+
+	//route middleware to verify a token
+	//routes written under this one will always go through this one
+	//So, others route ( under that middleware one) will need a token in header x-access-token
+	//you need to invoke the previous route to get a token
+	apiRoutes.use(function(req, res, next) {
+		// check header or url parameters or post parameters for token
+		var token = req.body.token || req.query.token || req.headers['x-access-token']
+		// decode token
+	 	if (token) {
+			// verifies secret and checks exp
+	    	jwt.verify(token, app.get('jwtSecret'), function(err, decoded) {      
+	      		if (err) {
+	        		return res.json({ success: false, message: 'Failed to authenticate token.' })    
+	      		} else {
+	        		// if everything is good, save to request for use in other routes
+	        		req.decoded = decoded    
+	        		next()
+	      		}
+	    	})
+		} else {
+			// if there is no token
+	    	// return an error
+	    	return res.status(403).send({ 
+	        	success: false, 
+	        	message: 'No token provided.' 
+	    	})
+	  	}
+	})
+
+	apiRoutes.get('/', function(req, res) {
+		//console.log("token decoded : " + req.decoded.id)
+	  	res.json({ message: 'Welcome to the petitsEmprunts API !' })
 	})
 
     //list all users
