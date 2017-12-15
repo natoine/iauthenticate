0424f69e71043a20da7389dcb11c728532a7b071
fatima221994 <fatima.machou@yahoo.com>
2017-12-15 12:01:58 +0100

    mis a jour des fichiers

--- config/auth.js
+++ config/auth.js
@@ -1,10 +1,10 @@
-
 // config/auth.js
 
 // expose our config directly to our application using module.exports
 module.exports = {
 
-    'facebookAuth' : {
+    'sessionsecret' : 'natoineiauthenticatesessionpassport', //passport needs this for sessions
+     'facebookAuth' : {
         'clientID'      : '', // your App ID
         'clientSecret'  : '', // your App Secret
         'callbackURL'   : 'http://localhost:8080/auth/facebook/callback'
@@ -20,6 +20,13 @@ module.exports = {
         'clientID'      : '',
         'clientSecret'  : '',
         'callbackURL'   : 'http://127.0.0.1:8080/auth/google/callback'
-    }
+    },
+	'APIAI_TOKEN' : '3', // your Client access token from dialogflow (Api.ai)
+	
+	'API_OPENWEATHER' : {
+        'consumerKey'       : '572a91d106726c152b30397f8298898f',
+    },
+	
+	//'API_OPENWEATHER':'572a91d106726c152b30397f8298898f'
 
 }
